a13288fe4e3aa2014c764ee5b0a90e57d98d3de6
Bruno Čale <50056973+brcale@users.noreply.github.com>
2020-09-20 14:11:45 +0200

    game renders but has no functionality

---
+++ src/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AuthorQuiz from './AuthorQuiz';
+import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn']
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.png',
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness']
+  },{
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Daniel Ogren',
+    books: ['Harry Potter and the Sorcerers']
+  },{
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stephenking.png',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Pinguino',
+    books: ['The Shinning', 'IT']
+  },{
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale of Two Cities']
+  },{
+    name: 'William Shakespeare',
+    imageUrl: 'images/authors/shakespeare.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+  }
+]
+
+function getTurnData(authors){
+  const allBooks = authors.reduce(function(p,c,i){
+    return p.concat (c.books);
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0,4);
+  const answer = sample(fourRandomBooks);
+  return {
+    books: fourRandomBooks,
+    author: authors.find((author) =>
+    author.books.some((title) => 
+    title===answer))
+  }
+}
+
+const state ={
+  turnData: getTurnData (authors)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthorQuiz {...state} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
